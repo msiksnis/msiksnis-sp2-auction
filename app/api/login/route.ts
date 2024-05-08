b1a050441c36f2cf6ca085f374a669467529b781
Martins Siksnis <msiksnis@gmail.com>
2024-05-08 18:34:17 +0200

    fixing a bug for 404 when logs in in production 5

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,3 +1,5 @@
+import { NextResponse } from "next/server";
+
 export async function POST(request: Request) {
   const body = await request.json();
   const { email, password } = body;
@@ -7,7 +9,6 @@ export async function POST(request: Request) {
     password,
   };
 
-  // Sends the data to the Noroff API for authentication
   try {
     const apiResponse = await fetch(`${process.env.API_BASE}/auth/login`, {
       method: "POST",
@@ -30,16 +31,15 @@ export async function POST(request: Request) {
 
     const data = await apiResponse.json();
 
-    // Return the response with user data without sensitive information
-    return new Response(JSON.stringify({ user: data }), {
-      status: 200,
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error: any) {
-    return new Response(JSON.stringify({ error: error.message }), {
-      status: 500,
-    });
+    return NextResponse.json({ user: data });
+    // return new Response(JSON.stringify({ user: data }), {
+    //   status: 200,
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    // });
+  } catch (error) {
+    console.error("[LOGIN_POST]", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
