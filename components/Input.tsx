6058b50cec201b50a37d70f531027b4f62464d49
Martins Siksnis <msiksnis@gmail.com>
2024-06-28 18:22:58 +0200

    fixed some types 2

--- components/Input.tsx
+++ components/Input.tsx
@@ -1,93 +1,50 @@
-import * as React from "react";
-
+import React, { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
 type InputProps = React.InputHTMLAttributes<HTMLInputElement> & {
   textarea?: false;
-<<<<<<< HEAD
   label?: string;
-=======
-  label?: string | null;
->>>>>>> 6f81d2ed69fc40b2e54f9ba91d41bca24f9ec54c
+  grow?: boolean;
+  flexBasis?: string;
 };
 
 type TextareaProps = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
   textarea: true;
   rows: number;
-<<<<<<< HEAD
   label?: string;
-=======
-  label?: string | null;
->>>>>>> 6f81d2ed69fc40b2e54f9ba91d41bca24f9ec54c
+  grow?: boolean;
+  flexBasis?: string;
 };
 
 type CustomInputProps = InputProps | TextareaProps;
 
-<<<<<<< HEAD
-export default function Input({
-  className,
-  textarea,
-  label,
-  ...props
-}: CustomInputProps) {
-  const inputClassName = cn(
-    "w-full rounded-md border-0 mt-1 py-1.5 px-2 shadow-sm ring-1 ring-inset ring-slate-300 placeholder:text-sm placeholder:text-slate-400 sm:text-sm sm:leading-6 focus:outline-none focus:border-black focus:ring-1 focus:ring-black",
-    className
-  );
-
-  // If 'label' prop is provided, use it as the label content. Otherwise, use the 'placeholder' prop for the label content.
-  const labelContent = label || props.placeholder;
-
-  // Conditionally render textarea or input based on the textarea prop. By default, it renders an input element.
-  if (textarea) {
-    const textareaProps = props as TextareaProps;
-    return (
-      <div>
-        <label htmlFor={textareaProps.id} className="text-sm font-medium">
-          {labelContent}
-        </label>
-        <textarea
-          {...textareaProps}
-          rows={textareaProps.rows || 3}
-          className={inputClassName}
-        />
-      </div>
-    );
-  } else {
-    const inputProps = props as InputProps;
-    return (
-      <div>
-        <label htmlFor={inputProps.id} className="text-sm font-medium">
-=======
-const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(
-  ({ className, textarea, label, ...props }, ref) => {
+const Input = forwardRef<
+  HTMLInputElement | HTMLTextAreaElement,
+  CustomInputProps
+>(
+  (
+    { className, textarea, label, grow = false, flexBasis = "auto", ...props },
+    ref
+  ) => {
     const inputClassName = cn(
-      "w-full rounded-md border-0 mt-1 py-1.5 px-2 shadow-sm ring-1 ring-inset ring-slate-300 placeholder:text-sm placeholder:text-slate-400 sm:text-sm sm:leading-6 focus:outline-none focus:border-black focus:ring-1 focus:ring-black",
+      "w-full rounded-md border-0 py-1.5 px-2 shadow-sm ring-1 ring-inset ring-slate-300 placeholder:text-sm placeholder:text-slate-400 sm:text-sm sm:leading-6 focus:outline-none focus:border-black focus:ring-1 focus:ring-black",
       className
     );
 
-    // Determines what is used as a label content
-    const labelContent = label !== undefined ? label : props.placeholder;
+    const wrapperClassName = cn(grow ? `flex-grow ${flexBasis}` : "");
 
-    // Conditionally renders the label only if 'label' is not explicitly an empty string
-    const labelElement =
-      label !== "" ? (
-        <label htmlFor={props.id} className="text-sm font-medium">
->>>>>>> 6f81d2ed69fc40b2e54f9ba91d41bca24f9ec54c
-          {labelContent}
-        </label>
-      ) : null;
+    const labelContent = label || props.placeholder;
 
-    // Conditionaly render textarea or input based on the textarea prop. By default, it renders an input element.
     if (textarea) {
       const textareaProps = props as TextareaProps;
       return (
-        <div>
+        <div className={wrapperClassName}>
           <label htmlFor={textareaProps.id} className="text-sm font-medium">
-            {labelElement}
+            {labelContent}
           </label>
           <textarea
             {...textareaProps}
+            ref={ref as React.Ref<HTMLTextAreaElement>}
             rows={textareaProps.rows || 3}
             className={inputClassName}
           />
@@ -96,11 +53,15 @@ const Input = React.forwardRef<HTMLInputElement, CustomInputProps>(
     } else {
       const inputProps = props as InputProps;
       return (
-        <div>
+        <div className={wrapperClassName}>
           <label htmlFor={inputProps.id} className="text-sm font-medium">
-            {labelElement}
+            {labelContent}
           </label>
-          <input {...inputProps} className={inputClassName} />
+          <input
+            {...inputProps}
+            ref={ref as React.Ref<HTMLInputElement>}
+            className={inputClassName}
+          />
         </div>
       );
     }
